069ba4bf2ed8fc279c8f0b30d18c77733876aa98
Jayesh Mishra <106644611+MishraJayesh@users.noreply.github.com>
2023-01-01 11:42:07 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -19,87 +19,87 @@ app.use(cors());
 
 //SignUp_Or_Register_APIs
 
-app.post("/register", async (req, resp) => {
+app.post("/register", async (req, res) => {
     let user = new User(req.body);
     let result = await user.save();
     result = result.toObject();
     delete result.password
     Jwt.sign({ result }, jwtKey, { expiresIn: "2h" }, (err, token) => {
         if (err) {
-            resp.send("Something Went Wrong");
+            res.send("Something Went Wrong");
         }
-        resp.send({ result, auth: token })
+        res.send({ result, auth: token })
     })
 })
 
 //Login_APIs
 
-app.post("/login", async (req, resp) => {
+app.post("/login", async (req, res) => {
     if (req.body.password && req.body.email) {
         let user = await User.findOne(req.body).select("-password");
         if (user) {
             Jwt.sign({ user }, jwtKey, { expiresIn: "2h" }, (err, token) => {
                 if (err) {
-                    resp.send("Something Went Wrong")
+                    res.send("Something Went Wrong")
                 }
-                resp.send({ user, auth: token })
+                res.send({ user, auth: token })
             })
         } else {
-            resp.send({ result: "No User Found" })
+            res.send({ result: "No User Found" })
         }
     } else {
-        resp.send({ result: "No User Found" })
+        res.send({ result: "No User Found" })
     }
 });
 
 //Product_APIs
 
-app.post("/add-product", verifyToken, async (req, resp) => {
+app.post("/add-product", verifyToken, async (req, res) => {
     let product = new Product(req.body);
     let result = await product.save();
-    resp.send(result);
+    res.send(result);
 });
 
-app.get("/products", verifyToken, async (req, resp) => {
+app.get("/products", verifyToken, async (req, res) => {
     const products = await Product.find();
     if (products.length > 0) {
-        resp.send(products)
+        res.send(products)
     } else {
-        resp.send({ result: "No Product Found" })
+        res.send({ result: "No Product Found" })
     }
 });
 
-app.delete("/product/:id", verifyToken, async (req, resp) => {
+app.delete("/product/:id", verifyToken, async (req, res) => {
     let result = await Product.deleteOne({ _id: req.params.id });
-    resp.send(result)
+    res.send(result)
 }),
 
-    app.get("/product/:id", verifyToken, async (req, resp) => {
+    app.get("/product/:id", verifyToken, async (req, res) => {
         let result = await Product.findOne({ _id: req.params.id })
         if (result) {
-            resp.send(result)
+            res.send(result)
         } else {
-            resp.send({ "result": "No Record Found." })
+            res.send({ "result": "No Record Found." })
         }
     })
 
-app.put("/product/:id", verifyToken, async (req, resp) => {
+app.put("/product/:id", verifyToken, async (req, res) => {
     let result = await Product.updateOne(
         { _id: req.params.id },
         { $set: req.body }
     )
-    resp.send(result)
+    res.send(result)
 });
 
-app.put("/product/:id", verifyToken, async (req, resp) => {
+app.put("/product/:id", verifyToken, async (req, res) => {
     let result = await Product.updateOne(
         { _id: req.params.id },
         { $set: req.body }
     )
-    resp.send(result)
+    res.send(result)
 });
 
-app.get("/search/:key", verifyToken, async (req, resp) => {
+app.get("/search/:key", verifyToken, async (req, res) => {
     let result = await Product.find({
         "$or": [
             {
@@ -113,7 +113,7 @@ app.get("/search/:key", verifyToken, async (req, resp) => {
             }
         ]
     });
-    resp.send(result);
+    res.send(result);
 })
 
 // Middleware
@@ -125,14 +125,14 @@ function verifyToken(req, res, next) {
         console.log("Middleware Is Called Inside Condition", token);
         Jwt.verify(token, jwtKey, (err, valid) => {
             if (err) {
-                resp.send({ result: "Please Provide Valid Token" });
+                res.send({ result: "Please Provide Valid Token" });
             } else {
                 next();
             }
         })
 
     } else {
-        resp.send({ result: "Please Add Token With Header" });
+        res.send({ result: "Please Add Token With Header" });
     }
 }
 
